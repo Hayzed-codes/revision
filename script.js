57de9025e30804762783c2aade0a78259f29e30d
Hayzed-codes <raheemhorlah@gmail.com>
2024-03-15 22:55:26 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,190 @@
+// // function Car(make, year, model, color) {
+// //     this.make = make;
+// //     this.year = year;
+// //     this.model = model;
+// //     this.color = color
+// // }
+
+// // const car1 = new Car("BMW", 2022, "Ibagaza", "Green")
+// // console.log(car1.model)
+
+
+// function task1(callback){
+//         setTimeout(() => {
+//             console.log("Task one is complete");
+//             callback();
+//         },5000);
+// }
+
+// function task2(callback){
+//         setTimeout(() => {
+//             console.log("Task two is complete");
+//             callback();
+//         },4000);
+// }
+
+// function task3(callback){
+//         setTimeout(() => {
+//             console.log("Task three is complete");
+//             callback();
+//         },3000);
+// }
+
+
+// // task1().then(value => {console.log(value)})
+// //        .then(value => {console.log(value)})
+// //        .then(value => {console.log(value); console.log("All task completed")});
+
+
+// // task1(() => {
+// //     task2(() => {
+// //         task3(() => {
+// //             console.log("All task completed")
+// //         })
+// //     })
+// // })
+
+// // task1();
+// // task2();
+// // task3();
+
+//     function cleanHouse (){
+        
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+    
+//                 const houseClean = true;
+    
+//                 if (houseClean){
+//                     resolve("The house got tidied up by me")
+//                 }else{
+//                     reject("He didn't clean the house")
+//                 }
+//             },3000)
+//         })
+//     };
+    
+//     function washCar (){
+//         return new Promise ((resolve, reject) => {
+//             setTimeout(() => {
+    
+//                 const carWashed = true;
+    
+//                 if (carWashed){
+//                     resolve("He has washed the car")
+//                 }else{
+//                     reject("He didn't wash the car")
+//                 }
+    
+//             },1000)
+            
+//         })
+//     };
+    
+//     function cookFood (){
+//         return new Promise ((resolve, reject) => {
+//             setTimeout(() => {
+    
+//                 const foodCooked = false;
+    
+//                 if (foodCooked){
+//                     resolve("The food is ready and served")
+//                 }else{
+//                     reject("He didn't cook the food")
+//                 }
+    
+//             },2000)
+//         })
+//     };
+
+// // async function allChores (){
+
+// //     try {
+// //         const cleanHouseResult = await cleanHouse();
+// //         console.log(cleanHouseResult);
+    
+// //         const washCarResult = await washCar();
+// //         console.log(washCarResult);
+    
+// //         const cookFoodResult = await cookFood();
+// //         console.log(cookFoodResult);
+// //         console.log("All tasks completed")
+// //     }
+// //     catch(error){
+// //         console.error(error)
+// //     }
+    
+// // }
+
+// // allChores();
+
+
+
+
+// cleanHouse().then(value => {console.log(value); return washCar()})
+//             .then(value => {console.log(value); return cookFood()})
+//             .then(value => {console.log(value); console.log("All task completed")})
+//             .catch(error => {console.error(error)});
+
+
+
+
+
+// fetch("https://pokeapi.co/api/v2/pokemon/mewtwo")
+//      .then(response => response.json())
+//      .then(data => console.log(data))
+//      .catch(error => (console.error(error)));
+
+/******************************** Fetch Data For Pokemon ************************/
+
+
+// fetchData();
+async function fetchData(){
+
+    try{
+
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+
+        if(!response.ok){
+            throw new Error ("Could not get the Response")
+        }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite")
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+
+        
+    }
+    catch(error){
+        console.error(error)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tAlli/script.js
@@ -0,0 +1,85 @@
+// // const demo = document.getElementById("demo")
+
+
+// function myDisplayer(something) {
+//     demo.innerHTML = something
+// }
+
+// function myCalculator (num1, num2, myCallback){
+//     let sum = num1 + num2
+//     myCallback(sum)
+// }
+
+// myCalculator(5, 5, myDisplayer);
+// myDisplayer()
+
+
+// setTimeout(myFunction, 3000);
+
+// function myFunction(){
+//     document.getElementById("love").innerHTML = "I love You"
+// }  
+
+// setTimeout(() => {
+    
+// }, timeout);
+
+// function yourFunction() {
+//     let d = new Date();
+
+//     document.getElementById("time").innerHTML = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+// }
+
+
+// setInterval(yourFunction, 5000)
+
+// function zainabu(some) {
+//     document.getElementById("asynku").innerHTML = some
+// }
+
+// async function thaFunction() {return "Hello"}
+
+// myFunction().then(
+//     function(value) {zainabu(value)},
+//     function(error) {zainabu(error)}
+// )
+
+
+// async function myDisplay(){
+//     let myPromise = new Promise(function(resolve){
+//         setTimeout(function() {resolve("I Love You")}, 3000)
+//     })
+//     document.getElementById("asynku").innerHTML = await myPromise;
+
+// }
+
+// myDisplay()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
